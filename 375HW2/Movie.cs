e5be67fe6924b676887611283e0668ba63c286ec
Eric Stokes <fernferret@gmail.com>
2010-03-23 23:27:50 -0400

    Refactor 8: Turning Conditional Logic into Polymorphism In this refactoring, I took two of the methods that currently reside within Rental and effectively moved the body of the code to the correct location, the Movie class.  Now GetCharge and GetFrequentRenterPoints both simply call the method from within Movie.  This makes sense, as the Information Expert tells us that the Movie class is the correct location to do this work.  This also, again cleans up the code a bunch and makes it much more readable, although this does add a few lines.  This also lessens the ripple effect if code is changed within the movie class, since the methods are now located and contained inside there.

--- 375HW2/Movie.cs
+++ 375HW2/Movie.cs
@@ -29,5 +29,34 @@ namespace CSSE375HW2
 		{
 			return _title;
 		}
+
+		public double GetCharge(int daysRented)
+		{
+			double result = 0;
+			switch (GetPriceCode())
+			{
+				// The qualifier, 'Movies' is redundant here
+				case Regular:
+					result += 2;
+					if (daysRented > 2)
+						result += (daysRented - 2)*1.5;
+					break;
+				case NewRelease:
+					result += daysRented*3;
+					break;
+				case Childrens:
+					result += 1.5;
+					if (daysRented > 3)
+						result += (daysRented - 3)*1.5;
+					break;
+			}
+			return result;
+		}
+		public int GetFrequentRenterPoints(int daysRented)
+		{
+			if ((GetPriceCode() == NewRelease) && daysRented > 1)
+				return 2;
+			return 1;
+		}
 	}
 }
--- 375HW2/Rental.cs
+++ 375HW2/Rental.cs
@@ -19,31 +19,11 @@
 		}
 		public double GetCharge()
 		{
-			double result = 0;
-			switch (GetMovie().GetPriceCode())
-			{
-				case Movie.Regular:
-					result += 2;
-					if (GetDaysRented() > 2)
-						result += (GetDaysRented() - 2) * 1.5;
-					break;
-				case Movie.NewRelease:
-					result += GetDaysRented() * 3;
-					break;
-				case Movie.Childrens:
-					result += 1.5;
-					if (GetDaysRented() > 3)
-						result += (GetDaysRented() - 3) * 1.5;
-					break;
-			}
-			return result;
+			return _movie.GetCharge(_daysRented);
 		}
 		public int GetFrequentRenterPoints()
 		{
-			if ((GetMovie().GetPriceCode() == Movie.NewRelease) && GetDaysRented() > 1)
-				return 2;
-			else
-				return 1;
+			return _movie.GetFrequentRenterPoints(_daysRented);
 		}
 	}
 }
